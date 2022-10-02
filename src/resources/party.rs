a58d44322c800d30a3e9899af6c76553c20d3451
Leandro Silva <leanoaks@gmail.com>
2022-10-01 23:52:38 -0300

    bringing necessary things into scope, also changed new_guy to new_member

--- src/resources/party.rs
+++ src/resources/party.rs
@@ -1,4 +1,6 @@
-struct Party {
+use crate::resources::{combatant::Combatant, item::Item};
+
+pub struct Party {
     members: Vec<Combatant>,
     items: Vec<Item>,
 }
@@ -9,8 +11,8 @@ impl Party {
             items,
         }
     }
-    fn recruit(&mut self, new_guy: Combatant) {
-        self.members.push(new_guy);
+    fn recruit(&mut self, new_member: Combatant) {
+        self.members.push(new_member);
     }
     fn store(&mut self, new_item: Item) {
         self.items.push(new_item);
