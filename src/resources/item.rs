5659fbb90c980c26528e6049d58d518b66201452
Leandro Silva <leanoaks@gmail.com>
2022-10-09 18:45:08 -0300

    moved the attrsmodifiers struct to here, declared the ItemInfo struct, formatting changes

--- src/resources/item.rs
+++ src/resources/item.rs
@@ -1,17 +1,33 @@
-
-#[derive (Copy, Clone)]
-enum Effect {
-
+#[derive(Copy, Clone)]
+struct AttrsModifiers {
+    hp: i16,
+    strength: i8,
+    constitution: i8,
+    intelligence: i8,
+    speed: i8,
+    luck: i8,
+}
+impl AttrsModifiers {
+    fn new() -> AttrsModifiers {
+        AttrsModifiers {
+            hp: 0,
+            strength: 0,
+            constitution: 0,
+            intelligence: 0,
+            speed: 0,
+            luck: 0,
+        }
+    }
 }
 
-#[derive (Clone)]
+#[derive(Clone)]
 pub enum Item {
     Equippable(Equippable),
     Letter(Letter),
     Consumable(Consumable),
 }
 
-#[derive (Copy, Clone)]
+#[derive(Copy, Clone)]
 pub enum EqpbleType {
     Weapon,
     Helmet,
@@ -19,25 +35,29 @@ pub enum EqpbleType {
     Accessory,
 }
 
-#[derive (Clone)]
-pub struct Equippable {
-    equip_type:  EqpbleType,
-    name:        String,
+#[derive(Clone)]
+struct ItemInfo {
+    name: String,
     description: String,
-    effects:     Effect,
 }
 
-#[derive (Clone)]
+#[derive(Clone)]
+pub struct Equippable {
+    equip_type: EqpbleType,
+    info: ItemInfo,
+    effects: AttrsModifiers,
+    weariness: Option<u16>,
+}
+
+#[derive(Clone)]
 pub struct Letter {
-    name:    String,
+    info: ItemInfo,
     message: String,
 }
 
-#[derive (Clone)]
+#[derive(Clone)]
 pub struct Consumable {
-    name:           String,
-    description:    String,
-    consume_times:  u8,
-    effect:         Effect
+    info: ItemInfo,
+    consume_times: u8,
+    effects: AttrsModifiers,
 }
-
