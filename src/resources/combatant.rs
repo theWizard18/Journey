0798b960df66c40fb317bab8df2b981a912bf672
Leandro Silva <leanoaks@gmail.com>
2022-10-09 18:42:47 -0300

    changed how the attributes are structured, also formatting changes

--- src/resources/combatant.rs
+++ src/resources/combatant.rs
@@ -1,126 +1,90 @@
+use crate::resources::item::{EqpbleType, Equippable};
 use crate::resources::party::Party;
-use crate::resources::item::{ Equippable, EqpbleType };
 
 const EXP_MAX: u32 = 1000000;
 
 pub struct Combatant {
-    name:       String,
-    party:      Option<Party>,
-    level:      u8,
-    exp:        u32,
+    name: String,
+    party: Option<Party>,
+    level: u8,
+    exp: u32,
     attributes: Attributes,
-    equipment:  Equipments,
+    equipment: Equipments,
 }
 impl Combatant {
     pub fn new(name: &str) -> Combatant {
         Combatant {
-            name:       name.into(),
-            party:      None,
-            level:      1,
-            exp:        0,
+            name: name.into(),
+            party: None,
+            level: 1,
+            exp: 0,
             attributes: Attributes::new(),
-            equipment:  Equipments::new(),
+            equipment: Equipments::new(),
         }
     }
 
     fn lv_up_threshold(&self) -> u32 {
         (self.level as u32 + 1).pow(3)
     }
-    
+
     fn increment_exp_by(&mut self, num: u32) {
         let sum = self.exp + num;
         self.exp = match sum <= EXP_MAX {
-            true  => sum,
+            true => sum,
             false => EXP_MAX,
         };
         while self.exp > self.lv_up_threshold() {
             self.level_up();
         }
     }
-    
+
     fn level_up(&mut self) {
         self.level += 1;
     }
 }
 
 struct Attributes {
-    default:    AttrsDefault,
-    modifiers:  AttrsModifiers,
+    hp: u16,
+    strength: u8,
+    constitution: u8,
+    intelligence: u8,
+    speed: u8,
+    luck: u8,
 }
 impl Attributes {
     fn new() -> Attributes {
         Attributes {
-            default:   AttrsDefault::new(),
-            modifiers: AttrsModifiers::new(),
-        }
-    }
-}
-
-struct AttrsDefault {
-    hp:           u16,
-    strength:     u8,
-    constitution: u8,
-    intelligence: u8,
-    speed:        u8,
-    luck:         u8,
-}
-impl AttrsDefault {
-    fn new() -> AttrsDefault {
-        AttrsDefault {
-            hp:           80,
-            strength:     20,
+            hp: 80,
+            strength: 20,
             constitution: 15,
             intelligence: 15,
-            speed:        18,
-            luck:         15,
-        }
-    }
-}
-
-#[derive (Default)]
-struct AttrsModifiers {
-    hp:           i16,
-    strength:     i8,
-    constitution: i8,
-    intelligence: i8,
-    speed:        i8,
-    luck:         i8,
-}
-impl AttrsModifiers {
-    fn new() -> AttrsModifiers {
-        AttrsModifiers {
-            hp:           0,
-            strength:     0,
-            constitution: 0,
-            intelligence: 0,
-            speed:        0,
-            luck:         0,
+            speed: 18,
+            luck: 15,
         }
     }
 }
 
 struct Equipments {
-    weapon:     Equippable,
-    head:       Equippable,
-    body:       Equippable,
-    accessory:  [Equippable; 4],
+    weapon: Equippable,
+    head: Equippable,
+    body: Equippable,
+    accessory: [Equippable; 4],
 }
 impl Equipments {
     fn new() -> Equipments {
         Equipments {
-            weapon:    Equippable::none(),
-            head:      Equippable::none(),
-            body:      Equippable::none(),
-            accessory: [Equippable::none(); 4]
+            weapon: Equippable::none(),
+            head: Equippable::none(),
+            body: Equippable::none(),
+            accessory: [Equippable::none(); 4],
         }
     }
-    
+
     fn equip_weapon(&mut self, item: &Equippable) {
         let buffer = self.weapon.clone();
         match item.equip_type {
             EqpbleType::Weapon => self.weapon = item.clone(),
-            _                  => println!("This item cannot be equipped as a Weapon")
+            _ => println!("This item cannot be equipped as a Weapon"),
         };
     }
 }
-
