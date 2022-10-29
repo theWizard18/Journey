1f7b4763a2b1443cc32af533955a04de08f74c44
Leandro Silva <leanoaks@gmail.com>
2022-10-29 17:53:44 -0300

    creates party and player then recruits player to party

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod game;
 mod resources;
 
-pub use crate::resources::combatant::Combatant;
+pub use crate::resources::{combatant::Combatant, party::Party};
 
 fn main() {
     print!("{}", termion::clear::All);
+    let mut party = Party::new(vec![], vec![]);
     let player = Combatant::new("Namir");
-    player.details();
+    party.recruit(player);
 }
