84ec675b6fe46772fc8f9c1a70a825a10989ea1a
Leandro Silva <leanoaks@gmail.com>
2022-11-24 23:13:52 -0300

    declared proficiencies struct, upgraded level up function, changed how attributes work

--- src/resources/combatant.rs
+++ src/resources/combatant.rs
@@ -1,4 +1,5 @@
 use crate::resources::item;
+use rand::{thread_rng, Rng};
 
 const EXP_MAX: u32 = 1000000;
 
@@ -8,6 +9,7 @@ pub struct Combatant {
     exp: u32,
     attributes: Attributes,
     equipment: Equipments,
+    proficiencies: Proficiencies,
 }
 impl Combatant {
     pub fn new(name: &str) -> Self {
@@ -17,6 +19,7 @@ impl Combatant {
             exp: 0,
             attributes: Attributes::new(),
             equipment: Equipments::new(),
+            proficiencies: Proficiencies::new(),
         }
     }
 
@@ -36,7 +39,15 @@ impl Combatant {
     }
 
     fn level_up(&mut self) {
-        self.level.saturating_add(1);
+        self.level += 1;
+        self.attributes.base_health.increment();
+        self.attributes.base_skill.increment();
+        self.attributes.base_strength.increment();
+        self.attributes.base_constitution.increment();
+        self.attributes.base_intelligence.increment();
+        self.attributes.base_luck.increment();
+        self.attributes.base_speed.increment();
+        self.attributes.base_accuracy.increment();
     }
 
     fn health(&self) -> u16 {
@@ -46,7 +57,7 @@ impl Combatant {
             .saturating_add(self.equipment.accessory_a.mod_attr.health)
             .saturating_add(self.equipment.accessory_b.mod_attr.health);
         let attr = self.attributes.base_health;
-        self.hp_sp_mod(attr, sum)
+        attr.attr_mod(sum)
     }
 
     fn skill(&self) -> u16 {
@@ -56,7 +67,7 @@ impl Combatant {
             .saturating_add(self.equipment.accessory_a.mod_attr.skill)
             .saturating_add(self.equipment.accessory_b.mod_attr.skill);
         let attr = self.attributes.base_skill;
-        self.hp_sp_mod(attr, sum)
+        attr.attr_mod(sum)
     }
 
     fn strength(&self) -> u8 {
@@ -66,7 +77,7 @@ impl Combatant {
             .saturating_add(self.equipment.accessory_a.mod_attr.strength)
             .saturating_add(self.equipment.accessory_b.mod_attr.strength);
         let attr = self.attributes.base_strength;
-        self.attr_mod(attr, sum)
+        attr.attr_mod(sum)
     }
 
     fn constitution(&self) -> u8 {
@@ -76,7 +87,7 @@ impl Combatant {
             .saturating_add(self.equipment.accessory_a.mod_attr.constitution)
             .saturating_add(self.equipment.accessory_b.mod_attr.constitution);
         let attr = self.attributes.base_constitution;
-        self.attr_mod(attr, sum)
+        attr.attr_mod(sum)
     }
 
     fn intelligence(&self) -> u8 {
@@ -86,7 +97,7 @@ impl Combatant {
             .saturating_add(self.equipment.accessory_a.mod_attr.intelligence)
             .saturating_add(self.equipment.accessory_b.mod_attr.intelligence);
         let attr = self.attributes.base_intelligence;
-        self.attr_mod(attr, sum)
+        attr.attr_mod(sum)
     }
 
     fn luck(&self) -> u8 {
@@ -96,7 +107,7 @@ impl Combatant {
             .saturating_add(self.equipment.accessory_a.mod_attr.luck)
             .saturating_add(self.equipment.accessory_b.mod_attr.luck);
         let attr = self.attributes.base_luck;
-        self.attr_mod(attr, sum)
+        attr.attr_mod(sum)
     }
 
     fn accuracy(&self) -> u8 {
@@ -106,7 +117,7 @@ impl Combatant {
             .saturating_add(self.equipment.accessory_a.mod_attr.accuracy)
             .saturating_add(self.equipment.accessory_b.mod_attr.accuracy);
         let attr = self.attributes.base_accuracy;
-        self.attr_mod(attr, sum)
+        attr.attr_mod(sum)
     }
 
     fn speed(&self) -> u8 {
@@ -116,51 +127,83 @@ impl Combatant {
             .saturating_add(self.equipment.accessory_a.mod_attr.speed)
             .saturating_add(self.equipment.accessory_b.mod_attr.speed);
         let attr = self.attributes.base_speed;
-        self.attr_mod(attr, sum)
+        attr.attr_mod(sum)
     }
+}
 
-    fn attr_mod(&self, attr: u8, modify: i8) -> u8 {
+#[derive(Copy, Clone)]
+struct Attr<T> {
+    value: T,
+}
+impl<T> Attr<T> {
+    fn new(value: T) -> Self {
+        Self {value}
+    }
+}
+impl Attr<u8> {
+    fn increment(&mut self) {
+        let mut rng = thread_rng();
+        let incr = 1 + ((self.value/100)*rng.gen_range(2..=5));
+        self.value = self.value.saturating_add(incr);
+    }
+    fn attr_mod(&self, modify: i8) -> u8 {
         match modify < 0 {
-            false => attr.saturating_add(modify as u8),
-            true => attr.saturating_sub(-modify as u8),
+            false => self.value.saturating_add(modify as u8),
+            true => self.value.saturating_sub(-modify as u8),
         }
     }
-
-    fn hp_sp_mod(&self, attr: u16, modify: i16) -> u16 {
+}
+impl Attr<u16> {
+    fn increment(&mut self) {
+        let mut rng = thread_rng();
+        let incr = 1 + ((self.value/100)*rng.gen_range(2..=5));
+        self.value = self.value.saturating_add(incr);
+    }
+    fn attr_mod(&self, modify: i16) -> u16 {
         match modify < 0 {
-            false => attr.saturating_add(modify as u16),
-            true => attr.saturating_sub(-modify as u16),
+            false => self.value.saturating_add(modify as u16),
+            true => self.value.saturating_sub(-modify as u16),
         }
     }
 }
 
 struct Attributes {
-    base_health: u16,
+    base_health: Attr<u16>,
     current_health: u16,
-    base_skill: u16,
+    base_skill: Attr<u16>,
     current_skill: u16,
-    base_strength: u8,
-    base_constitution: u8,
-    base_intelligence: u8,
-    base_luck: u8,
-    base_accuracy: u8,
-    base_speed: u8,
+    base_strength: Attr<u8>,
+    base_constitution: Attr<u8>,
+    base_intelligence: Attr<u8>,
+    base_luck: Attr<u8>,
+    base_accuracy: Attr<u8>,
+    base_speed: Attr<u8>,
 }
 impl Attributes {
     fn new() -> Self {
         Self {
-            base_health: 80,
+            base_health: Attr::new(80_u16),
             current_health: 80,
-            base_skill: 14,
+            base_skill: Attr::new(14_u16),
             current_skill: 14,
-            base_strength: 20,
-            base_constitution: 15,
-            base_intelligence: 15,
-            base_luck: 15,
-            base_accuracy: 20,
-            base_speed: 18,
+            base_strength: Attr::new(20_u8),
+            base_constitution: Attr::new(15_u8),
+            base_intelligence: Attr::new(15_u8),
+            base_luck: Attr::new(15_u8),
+            base_accuracy: Attr::new(20_u8),
+            base_speed: Attr::new(18_u8),
         }
     }
+    fn base_health(&self) -> u16 {self.base_health.value}
+    fn current_health(&self) -> u16 {self.current_health}
+    fn base_skill(&self) -> u16 {self.base_skill.value}
+    fn current_skill(&self) -> u16 {self.current_skill}
+    fn base_strength(&self) -> u8 {self.base_strength.value}
+    fn base_constitution(&self) -> u8 {self.base_accuracy.value}
+    fn base_intelligence(&self) -> u8 {self.base_intelligence.value}
+    fn base_luck(&self) -> u8 {self.base_luck.value}
+    fn base_accuracy(&self) -> u8 {self.base_accuracy.value}
+    fn base_speed(&self) -> u8 {self.base_speed.value}
 }
 
 struct Equipments {
@@ -182,5 +225,21 @@ impl Equipments {
     }
 
     fn equip_weapon(&mut self, item: &item::Weapon) {
+    }
+}
+
+#[derive(Default)]
+struct Proficiencies {
+    sword: u8,
+    axe: u8,
+    lance: u8,
+    staff: u8,
+    knife: u8,
+    hammer: u8,
+    shield: u8,
+}
+impl Proficiencies {
+    fn new() -> Self {
+        Default::default()
     }
 }
